e41800520f2a6f285dec0b722a262900a6debbd2
Vlad Klymenko <klymenvlad@gmail.com>
2023-10-28 13:37:05 +0300

    preparation for release

--- utils/baseUrl.ts
+++ utils/baseUrl.ts
@@ -1,9 +1,9 @@
 export const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3001"
-    : "https://dull-red-bat-slip.cyclic.app";
+    : "https://chatapp-backend-three.vercel.app";
 
 export const frontendUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "https://newmarket-shop.vercel.app";
+    : "https://chatapp-backend-three.vercel.app";
